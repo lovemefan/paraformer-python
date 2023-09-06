827ac17e035f6668d3985a42e9e1bbbcb0d60a14
lovemefan <lovemefan@outlook.com>
2023-09-06 22:31:10 +0800

    add vad offline

---
+++ test/test_vad_offline.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# @FileName  :test_vad_offline.py
+# @Time      :2023/9/6 16:19
+# @Author    :lovemefan
+# @Email     :lovemefan@outlook.com
+from paraformerOnline import FSMNVad, AudioReader
+
+if __name__ == '__main__':
+    speech, sample_rate = AudioReader.read_wav_file("test/vad_example.wav")
+    speech_length = speech.shape[0]
+    vad = FSMNVad()
+
+    result = vad.segments_offline(speech)
+    print(result)
