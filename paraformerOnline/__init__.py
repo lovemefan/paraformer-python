9d36084ac1157cb487cf743080569180bf61ccab
lovemefan <lovemefan@outlook.com>
2023-09-06 22:39:35 +0800

    add vad offline onnx import module

--- paraformerOnline/__init__.py
+++ paraformerOnline/__init__.py
@@ -7,20 +7,22 @@ import os
 
 from .runtime.python.asr_all_in_one import AsrAllInOne
 from .runtime.python.cttPunctuator import CttPunctuator
-from .runtime.python.fsmnVadInfer import FSMNVadOnline
+from .runtime.python.fsmnVadInfer import FSMNVadOnline, FSMNVad
 from .runtime.python.paraformerInfer import ParaformerOffline, ParaformerOnline
 from .runtime.python.svInfer import SpeakerVerificationInfer
 from .runtime.python.utils.audioHelper import AudioReader
-from .runtime.python.utils.logger import DEFAULT_FILEHANDLER_FORMAT, DEFAULT_STDOUT_FORMAT
+from .runtime.python.utils.logger import (DEFAULT_FILEHANDLER_FORMAT,
+                                          DEFAULT_STDOUT_FORMAT)
 
 __all__ = [
     "ParaformerOnline",
     "ParaformerOffline",
     "AsrAllInOne",
+    "FSMNVad",
     "FSMNVadOnline",
     "CttPunctuator",
     "SpeakerVerificationInfer",
     "AudioReader",
     "DEFAULT_FILEHANDLER_FORMAT",
-    "DEFAULT_STDOUT_FORMAT"
+    "DEFAULT_STDOUT_FORMAT",
 ]
