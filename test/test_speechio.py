25df9c387a9313dbacee309d08d98b9d2226e72e
lovemefan <lovemefan@outlook.com>
2023-09-09 15:21:39 +0800

    add rts test

--- test/test_speechio.py
+++ test/test_speechio.py
@@ -5,12 +5,12 @@
 # @Email     :lovemefan@outlook.com
 import argparse
 import json
-
-from jiwer import cer
 import logging.handlers
 import os.path
 import sys
+import time
 
+from jiwer import cer
 from tqdm import tqdm
 
 from paraformerOnline import AudioReader, ParaformerOffline
@@ -22,11 +22,13 @@ DEFAULT_FORMAT = (
 )
 
 parse = argparse.ArgumentParser()
-parse.add_argument('-i', '--input', required=True, help='input wav file or dir of wav files')
-parse.add_argument('-n', '--name', required=True)
+parse.add_argument(
+    "-i", "--input", required=True, help="input wav file or dir of wav files"
+)
+parse.add_argument("-n", "--name", required=True)
 args = parse.parse_args()
 
-logger = logging.getLogger('paraformer cer test')
+logger = logging.getLogger("paraformer cer test")
 logger.parent = None
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setLevel(logging.INFO)
@@ -35,7 +37,9 @@ stream_handler.setFormatter(stream_formatter)
 logger.addHandler(stream_handler)
 
 file_formatter = logging.Formatter(DEFAULT_FORMAT)
-file_handler = logging.handlers.RotatingFileHandler(filename=os.path.join(_LOG_FILE_DIR, f"{args.name}.txt"))
+file_handler = logging.handlers.RotatingFileHandler(
+    filename=os.path.join(_LOG_FILE_DIR, f"{args.name}.txt")
+)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -50,34 +54,47 @@ def cal_cer(config):
 
     transcripts = dict()
     logger.info("reading trans.txt")
-    with open(os.path.join(input, 'trans.txt'), 'r', encoding='utf-8') as file:
+    with open(os.path.join(input, "trans.txt"), "r", encoding="utf-8") as file:
         for line in file:
-            id, text = line.split('\t')
-            transcripts[id] = {'text': text.strip()}
+            id, text = line.split("\t")
+            transcripts[id] = {"text": text.strip()}
 
     logger.info("reading wav.scp")
-    with open(os.path.join(input, 'wav.scp'), 'r', encoding='utf-8') as file:
+    with open(os.path.join(input, "wav.scp"), "r", encoding="utf-8") as file:
         for line in file:
-            id, path = line.split('\t')
-            transcripts[id]['path'] = path.strip()
+            id, path = line.split("\t")
+            transcripts[id]["path"] = path.strip()
 
     _cer_all = 0
+    audio_time_total = 0
+    transcript_time_total = 0
     for id, info in tqdm(transcripts.items()):
-        audio, sample_rate = AudioReader.read_wav_file(os.path.join(input, info['path']))
-        text = info['text'].replace(' ', '')
-        hypothesis = model.infer_offline(audio)
-        error = cer(text, hypothesis)
-        transcripts[id]['hypothesis'] = hypothesis
-        transcripts[id]['error'] = error
+        audio, sample_rate = AudioReader.read_wav_file(
+            os.path.join(input, info["path"])
+        )
+        audio_time_total += len(audio) / 16000
 
-        logger.info(f"id:{id} path: {info['path']}, ground_truth: {text} , hypothesis: {hypothesis}, cer:{error}")
+        text = info["text"].replace(" ", "")
+
+        start = time.time()
+        hypothesis = model.infer_offline(audio)
+        transcript_time_total += time.time() - start
+        error = cer(text, hypothesis)
+
+        transcripts[id]["hypothesis"] = hypothesis
+        transcripts[id]["error"] = error
+
+        logger.info(
+            f"id:{id} path: {info['path']}, ground_truth: {text} , hypothesis: {hypothesis}, cer:{error}"
+        )
         _cer_all += error
 
     logger.info(f"all cer is :{_cer_all/len(transcripts.keys())}")
+    logger.info(f"rtf is :{transcript_time_total / audio_time_total}")
 
-    with open(f'{config.name}.json', 'w', encoding='utf-8') as file:
+    with open(f"{config.name}.json", "w", encoding="utf-8") as file:
         json.dump(transcripts, file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cal_cer(args)
