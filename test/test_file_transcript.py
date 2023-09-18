a398c3d1ad7d7c1e6a719d03f4fa1830275c8d7c
lovemefan <lovemefan@outlook.com>
2023-09-18 17:18:53 +0800

    add test file transcription test

---
+++ test/test_file_transcript.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+# @FileName  :test_file_transcript.py
+# @Time      :2023/9/18 16:38
+# @Author    :lovemefan
+# @Email     :lovemefan@outlook.com
+import logging
+
+from paraformerOnline import AsrAllInOne, AudioReader
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s %(levelname)s] [%(filename)s:%(lineno)d %(module)s.%(funcName)s] %(message)s",
+)
+
+if __name__ == "__main__":
+    logging.info("Testing online asr")
+    wav_path = "vad_example.wav"
+    speech, sample_rate = AudioReader.read_wav_file(wav_path)
+
+    model = AsrAllInOne(
+        mode="file_transcription",
+        speaker_verification=True,
+        sv_threshold=0.75,
+        sv_model_name="eres2net-quant",
+        hot_words="任意热词 空格隔开",
+    )
+
+    results = model.file_transcript(speech)
+
+    for i in results:
+        print(i)
